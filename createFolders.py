19f69ed193781923f491967ee504d94ccd1660fc
Ying WANG <ying.wang@grafana.com>
2022-11-09 10:12:52 +0100

    adding more char since i am lazy to handle collision

--- createFolders.py
+++ createFolders.py
@@ -48,7 +48,7 @@ def generateChildren(conn, parent_uid, dept, treeWidth):
     if dept == 0: return
     dept = dept - 1
     for i in range(treeWidth):
-        uid = lower_string(10)
+        uid = dept + '.' + treeWidth +lower_string(5)
         title = lower_string(10)
         f = (uid, 1, title, parent_uid, datetime.now(), datetime.now());
         create_folder(conn, f)
@@ -56,7 +56,7 @@ def generateChildren(conn, parent_uid, dept, treeWidth):
 
 def main():
     dept = 4
-    treeWidth = 100
+    treeWidth = 10
     database = "/Users/ying-jeanne/Workspace/grafana/data/grafana.db"
 
     # create a database connection
